fd8fdb6248dd7574f12e1a9187d8d288a3c4f9d9
元 <89688073+bfzg@users.noreply.github.com>
2024-01-28 21:11:03 +0800

    学习基础布局，组件传参，封装，统一颜色配置

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:learn_flutter/widgets/big_text.dart';
+
+import '../utils/colors.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({super.key});
@@ -10,24 +14,42 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Column(
-            children: [
-
-            ],
-          ),
-          Container(
-            width: 45,
-            height: 45,
-            decoration: BoxDecoration(
-             borderRadius: BorderRadius.circular(15),
-
-            ),
-          )
-        ],
-      ),
-    )
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+            child: Container(
+                margin: EdgeInsets.only(top: 45, bottom: 15),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        BigText(text: '我是餐厅标题'),
+                        BigText(
+                          text: '我是区域',
+                          color: AppColors.textColor,
+                          size: 12,
+                        )
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        width: 45,
+                        height: 45,
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                            color: AppColors.mainColor),
+                      ),
+                    )
+                  ],
+                ))),
+      ],
+    ));
   }
 }
---
+++ lib/widgets/big_text.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  final Color color;
+  final String text;
+  double size;
+  TextOverflow overFlow;
+
+  BigText(
+      {super.key,
+      this.color = const Color(0xFF5c524f),
+      required this.text,
+      this.size = 20,
+      this.overFlow = TextOverflow.ellipsis});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      overflow: overFlow,
+      style: TextStyle(
+          fontFamily: 'Roboto',
+          color: color,
+          fontSize: size,
+          fontWeight: FontWeight.w400),
+    );
+  }
+}
